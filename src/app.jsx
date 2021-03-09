1b103eb784fc0285222f7595f99ccde9216dddc4
jjtsou <john.tsourtis@camelotls.com>
2021-03-09 16:17:40 +0200

    Adding global styles

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,15 @@
 import ReactDom from 'react-dom';
-import Foobar from './foobar/Foobar';
+import GlobalStyle from './GlobalStyle';
+import StyledApp from './app.styles';
+import Title from './components/typography/Typography';
 
-const App = () => <Foobar />;
+const App = () => (
+  <StyledApp>
+    <GlobalStyle />
+    <Title tag="h2" fontSize={32}>
+      Cryptobook
+    </Title>
+  </StyledApp>
+);
 
 ReactDom.render(<App />, document.getElementById('root'));
