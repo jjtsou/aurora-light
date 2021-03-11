627622bf6dcdb9762a701f9740b1b51f55b23064
jjtsou <john.tsourtis@camelotls.com>
2021-03-11 22:01:36 +0200

    Adding getChartData and getCoinDetailsApi

--- src/api/coinGecko.js
+++ src/api/coinGecko.js
@@ -7,16 +7,33 @@ const api = axios.create({
 
 const endpoints = {
   coins_markets: '/coins/markets',
+  coin_market_chart: (id) => `/coins/${id}/market_chart`,
+  coin_details: (id) => `/coins/${id}`,
 };
 
 export const getCoins = (page) =>
-  api
-    .get(endpoints.coins_markets, {
-      params: {
-        vs_currency: 'usd',
-        per_page: 12,
-        page,
-      },
-    })
-    .then((response) => response.data)
-    .catch((error) => console.error(error));
+  api.get(endpoints.coins_markets, {
+    params: {
+      vs_currency: 'usd',
+      per_page: 12,
+      page,
+    },
+  });
+
+export const getChartData = (id, days) =>
+  api.get(endpoints.coin_market_chart(id), {
+    params: {
+      id,
+      vs_currency: 'usd',
+      days,
+    },
+  });
+
+export const getCoinDetails = (id) =>
+  api.get(endpoints.coin_details(id), {
+    params: {
+      id,
+      localization: 'false',
+      tickers: false,
+    },
+  });
